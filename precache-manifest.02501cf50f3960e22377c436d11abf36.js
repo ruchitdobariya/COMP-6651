46e79866cebde77433e983389541de65bb3febd2
ruchitdobariya <ruchitdobariya@gmail.com>
2024-02-10 20:29:38 -0500

    Updates

---
+++ precache-manifest.02501cf50f3960e22377c436d11abf36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee948992937b23dcab90ef24d6d1e1ec",
+    "url": "/COMP-6651/index.html"
+  },
+  {
+    "revision": "e032b5b8dea9f3d0c027",
+    "url": "/COMP-6651/static/css/main.100cc39f.chunk.css"
+  },
+  {
+    "revision": "d9093f4ab3a20444317e",
+    "url": "/COMP-6651/static/js/2.31569409.chunk.js"
+  },
+  {
+    "revision": "999f1045330027b1dba278ed88c8af72",
+    "url": "/COMP-6651/static/js/2.31569409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e68d1749741b16171d33",
+    "url": "/COMP-6651/static/js/3.902cda6e.chunk.js"
+  },
+  {
+    "revision": "e032b5b8dea9f3d0c027",
+    "url": "/COMP-6651/static/js/main.e939eb56.chunk.js"
+  },
+  {
+    "revision": "f78fd95ba0aab130145d",
+    "url": "/COMP-6651/static/js/runtime-main.f34d5121.js"
+  }
+]);
